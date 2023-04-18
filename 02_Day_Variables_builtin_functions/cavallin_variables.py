8a1b318e4e846456f7ccdaf3484dce722196f72a
JuanCavallin <61669937+JuanCavallin@users.noreply.github.com>
2023-04-17 23:23:19 -0400

    Create cavallin_variables.py

---
+++ 02_Day_Variables_builtin_functions/cavallin_variables.py
@@ -0,0 +1,18 @@
+#Day 2: 30 Days of python programming
+fn = "juan"
+ln = "cavallin"
+fullname = "Juan Andres Cavallin"
+country = "Venezuela"
+city = "Caracas"
+age = 17
+year = 2023
+married = false
+is_true = true
+is_light_on = true
+grade_level, ACT, SAT = "Junior", 35, 1530
+type(fn, fullname, age, SAT)
+len(fn)
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_one - num_two
